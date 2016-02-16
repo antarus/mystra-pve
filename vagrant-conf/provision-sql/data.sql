43e01a6fd9bba5fe26a0c6e81e9b922669508f36
antarus <antarus74@gmail.com>
2016-02-16 23:15:13 +0100

    Ajout donnée wow
    Ajout donné sql pour wow
    ajout css, img specifique wow
    correction layout

--- vagrant-conf/provision-sql/data.sql
+++ vagrant-conf/provision-sql/data.sql
@@ -3,17 +3,34 @@ INSERT INTO `classes` (`idClasses`, `couleur`, `nom`, `icon`) VALUES
 (2, '#F48CBA', 'Paladin', NULL),
 (3, '#AAD372', 'Chasseur', NULL),
 (4, '#FFF468', 'Voleur', NULL),
-(5, '#AAAAAA', 'Prêtre', NULL),
+(5, '#AAAAAA', 'Prï¿½tre', NULL),
 (6, '#C41E3B', 'Chevalier de la mort', NULL),
 (7, '#2359FF', 'Chaman', NULL),
 (8, '#68CCEF', 'Mage', NULL),
-(9, '#9382C9', 'Démoniste', NULL),
+(9, '#9382C9', 'Dï¿½moniste', NULL),
 (10, '#008467', 'Moine', NULL),
 (11, '#FF7C0A', 'Druide', NULL);
 
+INSERT INTO `race` (`idRace`, `nom`) VALUES
+(1, 'Humain'),
+(2, 'Orc'),
+(3, 'Nain'),
+(4, 'Elfe de la nuit'),
+(5, 'Mort-vivant'),
+(6, 'Tauren'),
+(7, 'Gnome'),
+(8, 'Troll'),
+(9, 'Gobelin'),
+(10, 'Elfe de sang'),
+(11, 'DraeneÃ¯'),
+(22, 'Worgen'),
+(24, 'Pandaren'),-- neutre
+(25, 'Pandaren'),-- alliance
+(26, 'Pandaren');-- horde
 
-
-
+INSERT INTO `faction` (`idFaction`, `nom`, `logo`) VALUES
+(1, 'Alliance', NULL),
+(2, 'Horde', NULL);
 
 
 
