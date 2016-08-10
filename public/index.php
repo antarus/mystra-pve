b8af1c9802ae997d69570faac21829e2ed65c330
antarus <antarus74@gmail.com>
2016-08-10 12:04:40 +0200

    modification index
    ajout commentaire de code suite a ajout apigility

--- public/index.php
+++ public/index.php
@@ -28,6 +28,6 @@ $appConfig = include APPLICATION_PATH . '/config/application.config.php';
 if (file_exists(APPLICATION_PATH . '/config/development.config.php')) {
     $appConfig = Zend\Stdlib\ArrayUtils::merge($appConfig, include APPLICATION_PATH . '/config/development.config.php');
 }
-
+//fin apigility
 // Run the application!
 Zend\Mvc\Application::init($appConfig)->run();
