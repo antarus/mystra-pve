7a98da61e4ae7fece04ed97276fb7bd764225cc9
kadyll <kadyll@kadyll-G73Jw>
2016-08-26 17:02:11 +0200

    Integration Backend

--- module/Backend/config/navigation.config.php
+++ module/Backend/config/navigation.config.php
@@ -5,105 +5,100 @@ $translator = new \Zend\I18n\Translator\Translator();
 return array(
     'default' => array(
         array(
-            'label' => $translator->translate('Home'),
+            'label' => $translator->translate('Accueil'),
             'route' => 'home',
             'action' => 'index',
         ),
         array(
-            'label' => $translator->translate('Référentiel'),
-            'route' => null,
+            'label' => $translator->translate('Boss'),
+            'route' => 'backend-bosses-list',
             'action' => 'index',
-            'pages' => array(
-                array(
-                    'label' => $translator->translate('Boss'),
-                    'route' => 'backend-bosses-list',
-                    'action' => 'index',
-                ),
-                array(
-                    'label' => $translator->translate('Classes'),
-                    'route' => 'backend-classes-list',
-                    'action' => 'index',
-                ),
-                array(
-                    'label' => $translator->translate('Evenements'),
-                    'route' => 'backend-evenements-list',
-                    'action' => 'index',
-                ),
-                array(
-                    'label' => $translator->translate('Faction'),
-                    'route' => 'backend-faction-list',
-                    'action' => 'index',
-                ),
-                array(
-                    'label' => $translator->translate('Guildes'),
-                    'route' => 'backend-guildes-list',
-                    'action' => 'index',
-                ),
-                array(
-                    'label' => $translator->translate('Items'),
-                    'route' => 'backend-items-list',
-                    'action' => 'index',
-                ),
-                array(
-                    'label' => $translator->translate('Items - personnage -raid - boss'),
-                    'route' => 'backend-item-personnage-raid-list',
-                    'action' => 'index',
-                ),
-                array(
-                    'label' => $translator->translate('Mode de difficulté'),
-                    'route' => 'backend-mode_difficulte-list',
-                    'action' => 'index',
-                ),
-                array(
-                    'label' => $translator->translate('Npc'),
-                    'route' => 'backend-npc-list',
-                    'action' => 'index',
-                ), array(
-                    'label' => $translator->translate('Pallier'),
-                    'route' => 'backend-pallier-afficher-list',
-                    'action' => 'index',
-                ),
-                array(
-                    'label' => $translator->translate('Personnages'),
-                    'route' => 'backend-personnages-list',
-                    'action' => 'index',
-                ),
-                array(
-                    'label' => $translator->translate('Race'),
-                    'route' => 'backend-race-list',
-                    'action' => 'index',
-                ),
-                array(
-                    'label' => $translator->translate('Raids'),
-                    'route' => 'backend-raids-list',
-                    'action' => 'index',
-                ),
-                array(
-                    'label' => $translator->translate('Role'),
-                    'route' => 'backend-role-list',
-                    'action' => 'index',
-                ),
-                array(
-                    'label' => $translator->translate('Roster'),
-                    'route' => 'backend-roster-list',
-                    'action' => 'index',
-                ),
-                array(
-                    'label' => $translator->translate('Spécialisation'),
-                    'route' => 'backend-specialisation-list',
-                    'action' => 'index',
-                ),
-                array(
-                    'label' => $translator->translate('Utilisateurs'),
-                    'route' => 'backend-users-list',
-                    'action' => 'index',
-                ),
-                array(
-                    'label' => $translator->translate('Zone'),
-                    'route' => 'backend-zone-list',
-                    'action' => 'index',
-                )
-            )
         ),
+        array(
+            'label' => $translator->translate('Classes'),
+            'route' => 'backend-classes-list',
+            'action' => 'index',
+        ),
+        array(
+            'label' => $translator->translate('Evenements'),
+            'route' => 'backend-evenements-list',
+            'action' => 'index',
+        ),
+        array(
+            'label' => $translator->translate('Faction'),
+            'route' => 'backend-faction-list',
+            'action' => 'index',
+        ),
+        array(
+            'label' => $translator->translate('Guildes'),
+            'route' => 'backend-guildes-list',
+            'action' => 'index',
+        ),
+        array(
+            'label' => $translator->translate('Items'),
+            'route' => 'backend-items-list',
+            'action' => 'index',
+        ),
+        array(
+            'label' => $translator->translate('Items - personnage -raid - boss'),
+            'route' => 'backend-item-personnage-raid-list',
+            'action' => 'index',
+        ),
+        array(
+            'label' => $translator->translate('Mode de difficulté'),
+            'route' => 'backend-mode_difficulte-list',
+            'action' => 'index',
+        ),
+        array(
+            'label' => $translator->translate('Npc'),
+            'route' => 'backend-npc-list',
+            'action' => 'index',
+        ), array(
+            'label' => $translator->translate('Pallier'),
+            'route' => 'backend-pallier-afficher-list',
+            'action' => 'index',
+        ),
+        array(
+            'label' => $translator->translate('Personnages'),
+            'route' => 'backend-personnages-list',
+            'action' => 'index',
+        ),
+        array(
+            'label' => $translator->translate('Race'),
+            'route' => 'backend-race-list',
+            'action' => 'index',
+        ),
+        array(
+            'label' => $translator->translate('Raids'),
+            'route' => 'backend-raids-list',
+            'action' => 'index',
+        ),
+        array(
+            'label' => $translator->translate('Role'),
+            'route' => 'backend-role-list',
+            'action' => 'index',
+        ),
+        array(
+            'label' => $translator->translate('Roster'),
+            'route' => 'backend-roster-list',
+            'action' => 'index',
+        ),
+        array(
+            'label' => $translator->translate('Spécialisation'),
+            'route' => 'backend-specialisation-list',
+            'action' => 'index',
+        ),
+        array(
+            'label' => $translator->translate('Utilisateurs'),
+            'route' => 'backend-users-list',
+            'action' => 'index',
+        ),
+        array(
+            'label' => $translator->translate('Zone'),
+            'route' => 'backend-zone-list',
+            'action' => 'index',
+        )
     )
+        
+    
 );
