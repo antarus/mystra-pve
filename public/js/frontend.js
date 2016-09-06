17941f038d092a255a8f79ab9c3512189e92ed59
Your Name <simoncreationweb@gmail.com>
2016-09-07 00:45:14 +0200

    debug affichage des stat general

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,14 +1,42 @@
 $(document).ready(function() {
     $("#buttonLoginLayoutFront").fancybox();
     
+    $.chartsVar = new Object();
+    
+    if($('#ajaxUrl').val())
+    {
+        $.post($('#ajaxUrl').val() + 'ajaxPresence')
+            .done(function( data ) {
+                $.chartsVar.Dez  = data.Dez;
+                $.chartsVar.Bank = data.Bank;
+                $.chartsVar.spe1 = data.spe1;
+                $.chartsVar.spe2 = data.spe2;
+                $.chartsVar.spe3 = data.spe3;
+                $.chartsVar.spe4 = data.spe4;
+            })
+            .fail(function() {
+                $.chartsVar.Dez  = 0;
+                $.chartsVar.Bank = 0;
+                $.chartsVar.spe1 = 0;
+                $.chartsVar.spe2 = 0;
+                $.chartsVar.spe3 = 0;
+                $.chartsVar.spe4 = 0;
+        });
+    }
+        
+    
     // charts
     google.charts.load("current", {packages:["corechart"]});
     google.charts.setOnLoadCallback(drawChart);
       function drawChart() {
         var data = google.visualization.arrayToDataTable([
-          ['Presence', '%'],
-          ['Ok',     90],
-          ['Abs',      10],
+          ['loot', '%'],
+          ['Dez',     $.chartsVar.Dez],
+          ['Bank',    $.chartsVar.Bank],
+          ['Spe 1',   $.chartsVar.spe1],
+          ['Spe 2',   $.chartsVar.spe2],
+          ['Spe 3',   $.chartsVar.spe3],
+          ['Spe 4',   $.chartsVar.spe4],
         ]);
 
         var options = {
